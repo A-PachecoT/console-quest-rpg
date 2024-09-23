48a931eff1054ce7146a5e29d2791fc023bb1ead
A-PachecoT <andre.pacheco.t@uni.pe>
2024-09-23 15:12:20 -0500

    update: init files

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,4 @@
 from .endpoints.character_routes import router as character_routes
-from .endpoints.dungeon_routes import router as dungeon_routes
 from .endpoints.general_routes import router as general_routes
 from .endpoints.player_routes import router as player_routes
 
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,2 @@
-from .character_service import CharacterService
 from .dungeon_service import DungeonService
 from .player_service import PlayerService
