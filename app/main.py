cf7e266c10a0fafe35b19da9029364efde889edd
thsergitox <sergiopezoj@gmail.com>
2024-09-28 22:34:45 -0500

    feat: finished authenticacion with jwt

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from app.database.mongo.connection import MongoConnection
 from app.config import settings
 from app.routes import router
 from prometheus_fastapi_instrumentator import Instrumentator
+from fastapi.staticfiles import StaticFiles
+from fastapi import Request
+import jwt
 
 # Creamos una instancia de la aplicación FastAPI
 app = FastAPI(
@@ -11,6 +14,20 @@ app = FastAPI(
     version="1.0.0"
 )
 
+@app.middleware("http")
+async def token_middleware(request: Request, call_next):
+    token = request.cookies.get("access_token")
+    if token:
+        try:
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+            request.state.user = payload.get("name")
+        except jwt.ExpiredSignatureError:
+            request.state.user = None
+    return await call_next(request)
+
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="app/views"), name="static")
 
 @app.on_event("startup")
 async def startup_db_client():
@@ -22,7 +39,7 @@ async def shutdown_db_client():
 
 
 # Incluimos el router de la aplicación con el prefijo "/api"
-app.include_router(router, prefix="/api")
+app.include_router(router)
 
 # Configuramos Prometheus para instrumentar y exponer métricas de la aplicación
 Instrumentator().instrument(app).expose(app)
