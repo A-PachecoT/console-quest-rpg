a364b03d95fa29c215dca3e2ac152c12269581eb
Max <oscar.torres.m@uni.pe>
2024-09-29 00:48:26 -0500

    resolve conflict

--- app/routes/endpoints/combat_routes.py
+++ app/routes/endpoints/combat_routes.py
@@ -43,89 +43,99 @@ async def combat_home(request: Request):
 
 
 @router.get("/start", response_model=dict)
-async def start_combat(
-    request: Request, playerService: PlayerService = Depends(get_player_service)
-):
-    user = getattr(request.state, "user", None)
-    if not user:
-        return RedirectResponse(url="/")
-
-    player = await playerService.get_player_by_name(user)
-    if not player:
-        return {"message": "Player does not exist"}
-
-    if player["current_enemy"]:
-        return {"message": "Combat already started"}
-
-    # TODO: Create enemy for the current player
-    return {"message": f"Combat started for {user}"}
-
+async def start_combat(request: Request, playerService: PlayerService = Depends(get_player_service)):
+	user = getattr(request.state, 'user', None)
+	if not user:
+		return RedirectResponse(url="/")
+	
+	player = await playerService.get_player_by_name(user)
+	if not player:
+		return {
+			"message": "Player does not exist"
+		}
+	
+	if player["current_enemy"]:
+		return {
+			"message": "Combat already started"
+		}
+	
+	#TODO: Create enemy for the current player
+	return {
+		"message": f"Combat started for {user}"
+	}
 
 @router.get("/attack", response_model=dict)
-async def attack(
-    request: Request, playerService: PlayerService = Depends(get_player_service)
-):
-    user = getattr(request.state, "user", None)
-    if not user:
-        return RedirectResponse(url="/")
+async def attack(request: Request, playerService: PlayerService = Depends(get_player_service)):
+	user = getattr(request.state, 'user', None)
+	if not user:
+		return RedirectResponse(url="/")
+	
+	player = await playerService.get_player_by_name(user)
+	if not player:
+		return {
+			"message": "Player does not exist"
+		}
+	
+	log = []
 
-    player = await playerService.get_player_by_name(user)
-    if not player:
-        return {"message": "Player does not exist"}
-
-    # TODO: Implement attack logic
-    return {"message": f"{user} attacked"}
-
+	log.append(player.TakeTurn(player["current_enemy"], 1))
+	enemyAction = random.randint(0, 1)
+	log.append(player["current_enemy"].TakeTurn(player, enemyAction))
+	
+	return {
+		"log": log
+	}
 
 @router.get("/defend", response_model=dict)
-async def defend(
-    request: Request, playerService: PlayerService = Depends(get_player_service)
-):
-    user = getattr(request.state, "user", None)
-    if not user:
-        return RedirectResponse(url="/")
-
-    player = await playerService.get_player_by_name(user)
-    if not player:
-        return {"message": "Player does not exist"}
-
-    # TODO: Implement defend logic
-    return {"message": f"{user} defended"}
-
+async def defend(request: Request, playerService: PlayerService = Depends(get_player_service)):
+	user = getattr(request.state, 'user', None)
+	if not user:
+		return RedirectResponse(url="/")
+	
+	player = await playerService.get_player_by_name(user)
+	if not player:
+		return {
+			"message": "Player does not exist"
+		}
+	
+	#TODO: Implement defend logic
+	return {
+		"message": f"{user} defended"
+	}
 
 @router.get("/ability", response_model=dict)
-async def ability_menu(
-    request: Request, playerService: PlayerService = Depends(get_player_service)
-):
-    user = getattr(request.state, "user", None)
-    if not user:
-        return RedirectResponse(url="/")
-
-    player = await playerService.get_player_by_name(user)
-    if not player:
-        return {"message": "Player does not exist"}
-
-    abilityList = player["abilities"]
-    return {
-        "message": f"{user} abilities",
-        "use": "/combat/ability/{ability_id}",
-        "abilities": abilityList,
-    }
+async def ability_menu(request: Request, playerService: PlayerService = Depends(get_player_service)):
+	user = getattr(request.state, 'user', None)
+	if not user:
+		return RedirectResponse(url="/")
+	
+	player = await playerService.get_player_by_name(user)
+	if not player:
+		return {
+			"message": "Player does not exist"
+		}
+	
+	abilityList = player["abilities"]
+	return {
+		"message": f"{user} abilities",
+		"use": "/combat/ability/{ability_id}",
+		"abilities": abilityList
+	}
 
 
 @router.get("/ability/{ability_id}", response_model=dict)
-async def use_ability(
-    request: Request,
-    ability_id: int,
-    playerService: PlayerService = Depends(get_player_service),
-):
-    user = getattr(request.state, "user", None)
-    if not user:
-        return RedirectResponse(url="/")
+async def use_ability(request: Request, ability_id: int, playerService: PlayerService = Depends(get_player_service)):
+	user = getattr(request.state, 'user', None)
+	if not user:
+		return RedirectResponse(url="/")
+	
+	player = await playerService.get_player_by_name(user)
+	if not player:
+		return {
+			"message": "Player does not exist"
+	}
 
-    player = await playerService.get_player_by_name(user)
-    if not player:
-        return {"message": "Player does not exist"}
-
-    # TODO: Implement ability logic
-    return {"message": f"{user} used ability {ability_id}"}
+	#TODO: Implement ability logic
+	return {
+		"message": f"{user} used ability {ability_id}"
+	}
